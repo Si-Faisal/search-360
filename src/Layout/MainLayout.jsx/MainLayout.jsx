08433794174196b7eaf46e6b77505953d364f3d3
Shahidul Islam Faisal <sifaisalcox@gmail.com>
2023-06-14 15:55:47 +0600

    main layout

--- src/Layout/MainLayout.jsx/MainLayout.jsx
+++ src/Layout/MainLayout.jsx/MainLayout.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
-import Header from '../../Pages/Header/Header';
-import Footer from '../../Pages/Footer/Footer';
+// import React from 'react';
+// import { Outlet, useLocation } from 'react-router-dom';
+// import Header from '../../Pages/Header/Header';
+// import Footer from '../../Pages/Footer/Footer';
 
-const MainLayout = () => {
-    const location = useLocation();
+// const MainLayout = () => {
+//     const location = useLocation();
 
-    const noHeaderFooter = location.pathname.includes('login') || location.pathname.includes('signup');
-    return (
-        <div>
-            {noHeaderFooter || <Header></Header>}
-            <Outlet></Outlet>
-            {noHeaderFooter || <Footer></Footer>}
-        </div>
-    );
-};
+//     const noHeaderFooter = location.pathname.includes('login') || location.pathname.includes('signup');
+//     return (
+//         <div>
+//             {noHeaderFooter || <Header></Header>}
+//             <Outlet></Outlet>
+//             {noHeaderFooter || <Footer></Footer>}
+//         </div>
+//     );
+// };
 
-export default MainLayout;
+// export default MainLayout;
