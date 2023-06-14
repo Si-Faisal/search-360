a17f786bd91ef2762b01136b4cef80257ddc58b3
Shahidul Islam Faisal <sifaisalcox@gmail.com>
2023-06-14 15:52:57 +0600

    instractor route

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -1,23 +1,23 @@
-// import { Navigate, useLocation } from "react-router";
+import { Navigate, useLocation } from "react-router";
 
-// import useAdmin from "../hooks/useAdmin";
-// import { useContext } from "react";
-// import { AuthContext } from "../AuthProvider/AuthProvider";
+import useAdmin from "../hooks/useAdmin";
+import { useContext } from "react";
+import { AuthContext } from "../AuthProvider/AuthProvider";
 
 
-// const AdminRoute = ({ children }) => {
-//     const { user, loading } = useContext(AuthContext);
-//     const [isAdmin, isAdminLoading] = useAdmin();
-//     const location = useLocation();
+const AdminRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+    const [isAdmin, isAdminLoading] = useAdmin();
+    const location = useLocation();
 
-//     if (loading || isAdminLoading) {
-//         return <progress className="progress w-56"></progress>
-//     }
+    if (loading || isAdminLoading) {
+        return <progress className="progress w-56"></progress>
+    }
 
-//     if (user && isAdmin) {
-//         return children;
-//     }
-//     return <Navigate to="/" state={{ from: location }} replace></Navigate>
-// };
+    if (user && isAdmin) {
+        return children;
+    }
+    return <Navigate to="/" state={{ from: location }} replace></Navigate>
+};
 
-// export default AdminRoute;
+export default AdminRoute;
