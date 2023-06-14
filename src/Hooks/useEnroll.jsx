796f9c7bf8690e21b7d5b984ebfb5b3a3a3e6ffe
Shahidul Islam Faisal <sifaisalcox@gmail.com>
2023-06-14 15:58:31 +0600

    use enroll

--- src/Hooks/useEnroll.jsx
+++ src/Hooks/useEnroll.jsx
@@ -1,31 +1,31 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../AuthProvider/AuthProvider';
-import { useQuery } from '@tanstack/react-query';
+// import React, { useContext } from 'react';
+// import { AuthContext } from '../AuthProvider/AuthProvider';
+// import { useQuery } from '@tanstack/react-query';
 
-const useEnroll = () => {
+// const useEnroll = () => {
 
-    const token = localStorage.getItem("access-token");
-    const { user, loading } = useContext(AuthContext);
-
-
-    const { refetch: enrollrefetch, loading: enrollLoading, data: isEnroll = [] } = useQuery({
-        queryKey: ['enroll'],
-        enabled: !!user?.email && !!localStorage.getItem("access-token"),
-        queryFn: async () => {
-            const res = await fetch("http://localhost:5000/class/enroll", {
-                method: "GET",
-                headers: {
-                    authorization: `bearar ${token} `
-                }
-            })
-            return res.json();
-        }
-
-    })
+//     const token = localStorage.getItem("access-token");
+//     const { user, loading } = useContext(AuthContext);
 
 
+//     const { refetch: enrollrefetch, loading: enrollLoading, data: isEnroll = [] } = useQuery({
+//         queryKey: ['enroll'],
+//         enabled: !!user?.email && !!localStorage.getItem("access-token"),
+//         queryFn: async () => {
+//             const res = await fetch("http://localhost:5000/class/enroll", {
+//                 method: "GET",
+//                 headers: {
+//                     authorization: `bearar ${token} `
+//                 }
+//             })
+//             return res.json();
+//         }
 
-    return [enrollrefetch, enrollLoading, isEnroll];
-};
+//     })
 
-export default useEnroll;
+
+
+//     return [enrollrefetch, enrollLoading, isEnroll];
+// };
+
+// export default useEnroll;
