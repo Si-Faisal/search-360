e393b9eed8d142facbef63f59c730997b6923fb5
Shahidul Islam Faisal <sifaisalcox@gmail.com>
2023-06-14 15:57:14 +0600

    use instractor

--- src/Hooks/useUserData.jsx
+++ src/Hooks/useUserData.jsx
@@ -1,29 +1,29 @@
-// import React, { useContext } from 'react';
-// import { AuthContext } from '../AuthProvider/AuthProvider';
-// import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../AuthProvider/AuthProvider';
+import { useQuery } from '@tanstack/react-query';
 
-// const useUserData = () => {
+const useUserData = () => {
 
-//     const token = localStorage.getItem("access-token");
-//     const { user } = useContext(AuthContext);
+    const token = localStorage.getItem("access-token");
+    const { user } = useContext(AuthContext);
 
-//     const { refetch: refetch, loading: isUserLoading, data: isUser = [] } = useQuery({
-//         queryKey: ['users'],
-//         enabled: !!user?.email && !!localStorage.getItem("access-token"),
-//         queryFn: async () => {
-//             const res = await fetch("http://localhost:5000/users", {
-//                 method: "GET",
-//                 headers: {
-//                     authorization: `bearar ${token} `
-//                 }
-//             })
-//             return res.json();
-//         }
+    const { refetch: refetch, loading: isUserLoading, data: isUser = [] } = useQuery({
+        queryKey: ['users'],
+        enabled: !!user?.email && !!localStorage.getItem("access-token"),
+        queryFn: async () => {
+            const res = await fetch("http://localhost:5000/users", {
+                method: "GET",
+                headers: {
+                    authorization: `bearar ${token} `
+                }
+            })
+            return res.json();
+        }
 
-//     })
+    })
 
-//     console.log(user);
-//     return [refetch, isUserLoading, isUser];
-// };
+    console.log(user);
+    return [refetch, isUserLoading, isUser];
+};
 
-// export default useUserData;
+export default useUserData;
